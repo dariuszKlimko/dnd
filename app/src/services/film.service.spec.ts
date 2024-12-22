b4f4552d292a6a4d82786c48e92b4d62eb19df13
dariuszKlimko <dariuszklimkoportfolio@gmail.com>
2024-12-22 15:21:30 +0100

    typeorm-fixtures-cli moved to devDep

--- app/src/services/film.service.spec.ts
+++ app/src/services/film.service.spec.ts
@@ -1,12 +1,6 @@
 import { Test, TestingModule } from "@nestjs/testing";
-import { GeneratorServiceIntrface } from "@app/common/types/interfaces/services/generator.service.interface";
-import { GeneratorSevice } from "@app/services/generator.service";
-import { JwtService } from "@nestjs/jwt";
 import { ConfigService } from "@nestjs/config";
-import { RESET_PASSWORD_MESSAGE, VERIFICATION_EMAIL_MESSAGE } from "@app/common/constans/constans";
-import { User } from "@app/entities/user.entity";
 import { faker } from "@faker-js/faker";
-import { JwtPayload } from "@app/common/types/type/jwt.payload";
 import { FilmServiceIntrface } from "@app/common/types/interfaces/services/film.service.interface";
 import { FilmService } from "@app/services/film.service";
 
